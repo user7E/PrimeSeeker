99b0f90804db3247a8fca111c546193f8fa372cd
Old User <122145414+user7E@users.noreply.github.com>
2023-03-14 18:15:20 -0300

    Add files via upload

---
+++ PrimeSeeker/CP.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+// My Algorithm for primes (based on Eratosthenes')
+//
+class CPA
+{
+    // Class members
+    public double num0, num1, num2, num3;        // field
+    
+    
+    public double checkPrimeD(double num0, double num1)   // method
+    {
+        double c;
+        c = num0 / num1;
+        return (c);
+    }
+}
