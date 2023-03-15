4dbb6343227da812e3d8579f4b5efae9399a1266
Old User <122145414+user7E@users.noreply.github.com>
2023-03-14 21:19:21 -0300

    Add files via upload

---
+++ src/Mult2et3.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrimeSeeker
+{
+    public class Mult2et3
+    {
+        //Values
+        int element0;
+
+        //Methods
+        public bool CheckPrimeA(int a)
+        {
+            int b;
+            b = a % 2;
+            if (b == 0 && b != 2)
+            {
+                Console.WriteLine("Não é primo!");
+                return false;
+            }
+
+            int b3;
+            b3 = a % 3;
+            if (b3 == 0 && b3 != 3)
+            {
+                Console.WriteLine("Não é primo!");
+                return false;
+            }
+            else
+            {
+                Console.WriteLine("Pode ser primo!");
+                return true;
+            }
+
+        }
+
+    }     
+}
---
+++ src/Program.cs
@@ -0,0 +1,48 @@
+﻿/*  Pequeno programa para achar e verificar números primos.
+ *  inspirado no Algoritmo de Eratóstenes(também conhecido como Crivo de Eratóstenees)
+ *  para encontrá-los aritmeticamente.
+ *
+ *  Written by Caio Cezar, 2023
+ *      PrimeSeeker
+ *       v0.2a
+ *       
+ *      *GPLv2
+*/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrimeSeeker
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // Elemento para verificação
+            int elementN;
+            // Versão String do Elemento
+            string strElementN;
+            //
+
+
+            Console.WriteLine("Inicio...");
+            Console.WriteLine("Insira um número natural: ");
+            strElementN = Console.ReadLine();
+            elementN = Int32.Parse(strElementN);
+            Console.WriteLine(elementN);
+            Mult2et3 n2a3 = new Mult2et3();
+            n2a3.CheckPrimeA(elementN);
+            Console.WriteLine("...");
+            Console.WriteLine();
+            Console.WriteLine("Fim...");
+
+
+            Console.ReadLine();
+        }
+    }
+}
